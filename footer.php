37ef43ac792b8c2a93a99fffecca318e177cceac
Soniya <soniya.n@somaiya.edu>
2021-11-11 21:32:58 +0530

    created user dashboard

---
+++ footer.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Footer</title>
+        <link rel="stylesheet" href="footer.css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="page-wrapper"></div>
+<div class="footer">
+<div class="footer-content">
+
+<div class="footer-section about">
+<h3><a href="#">Online Birth Certificate
+<br>Form Filling & Documentation by Email</a></h3><br><br><br>
+</div> 
+
+<div class="footer-section contact">
+<h2>Contact Us</h2>
+<span><i class="fa fa-envelope"></i><a href="#"> info@birthcertificate.org.in</a></span><br><br>
+<span><i class="fa fa-phone"></i><a href="#"> +91 7875048191</a></span>
+</div>
+
+<div class="footer-section links">
+<h2>Quick Links</h2>
+<ul>
+    <a href="#"><li>Terms & Conditions</li></a>
+    <a href="#"><li>Privacy Policy</li></a>
+    <a href="#"><li>Refund Policy</li></a>
+    <a href="#"><li>Contact Us</li></a>
+</ul>
+</div>
+<div class="footer-section discl">
+<p style="font-size:22px">The disclaimer as read follows<p>
+    <p>The company being service provider only deals with processing of applications on behalf of the applicants
+    And as such not responsible for fees structure to be paid by the applicants to the authorities. The fees to us
+    since are only processing fees they are not refundable</p>
+</div>
+</div>
+<div class="footer-bottom">
+    &copy; 2021 online birth certificate
+</div>
+</div>
+</body>
+</html>
