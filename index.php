219da4867223727b2cf48c9becdc82ae4d1abf9a
Sanika <sanika.dk@somaiya.edu>
2021-12-11 20:06:08 +0530

    :sparkles: Created Home Page

--- index.php
+++ index.php
@@ -14,79 +14,66 @@
     <link rel="stylesheet " href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i " />
     <script src="https://kit.fontawesome.com/a07eba2ffc.js " crossorigin="anonymous "></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script type="text/javascript">
-      var counter = 1;
-      setInterval(function(){
-       document.getElementById('radio' + counter).checked = true;
-        counter++;
-      if(counter > 4)
-       counter = 1;
-    },5000);
-</script>
+        var counter = 1;
+        setInterval(function() {
+            document.getElementById('radio' + counter).checked = true;
+            counter++;
+            if (counter > 4)
+                counter = 1;
+        }, 5000);
+    </script>
 </head>
 
 
 <body>
 
     <?php include "navbar.php" ?>
-    
+
     <main class="img-slider">
-<!-- Image Slider -->
-<div class="slider">
-<div class="slides">
-<!-- Radio Button-->
-<input type="radio" name="radbtn" id="radio1">
-<input type="radio" name="radbtn" id="radio2">
-<input type="radio" name="radbtn" id="radio3">
-<input type="radio" name="radbtn" id="radio4">
-<!-- Slide Image -->
-<div class="slide first">
-<img src = "https://images.pexels.com/photos/3376801/pexels-photo-3376801.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="birthcertificate">
-</div>
-<div class="slide">
-<img src ="https://media.them.us/photos/608897503827ab07009db527/master/w_2560%2Cc_limit/GettyImages-97684140.jpg">
-</div>
-<div class="slide">
-<img src = "https://images.pexels.com/photos/208189/pexels-photo-208189.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
-</div>
-<div class="slide">
-<img src = "https://media.istockphoto.com/photos/family-papers-picture-id172437046?k=20&m=172437046&s=612x612&w=0&h=JmSAlG0QuIVbPyhHo0arQsa7tpTMCg5xHO5HpRBwrSA=">
-</div>
+        <!-- Image Slider -->
+        <div class="slider">
+            <div class="slides">
+                <!-- Radio Button-->
+                <input type="radio" name="radbtn" id="radio1">
+                <input type="radio" name="radbtn" id="radio2">
+                <input type="radio" name="radbtn" id="radio3">
+                <input type="radio" name="radbtn" id="radio4">
+                <!-- Slide Image -->
+                <div class="slide first">
+                    <img src="https://images.pexels.com/photos/3376801/pexels-photo-3376801.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="birthcertificate">
+                </div>
+                <div class="slide">
+                    <img src="https://media.them.us/photos/608897503827ab07009db527/master/w_2560%2Cc_limit/GettyImages-97684140.jpg">
+                </div>
+                <div class="slide">
+                    <img src="https://images.pexels.com/photos/208189/pexels-photo-208189.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
+                </div>
+                <div class="slide">
+                    <img src="https://media.istockphoto.com/photos/family-papers-picture-id172437046?k=20&m=172437046&s=612x612&w=0&h=JmSAlG0QuIVbPyhHo0arQsa7tpTMCg5xHO5HpRBwrSA=">
+                </div>
 
-<!-- Automatic nav start -->
-<div class="navgate-auto">
-<div class="auto-btn1"></div>
-<div class="auto-btn2"></div>
-<div class="auto-btn3"></div>
-<div class="auto-btn4"></div>
-</div>
+                <!-- Automatic nav start -->
+                <div class="navgate-auto">
+                    <div class="auto-btn1"></div>
+                    <div class="auto-btn2"></div>
+                    <div class="auto-btn3"></div>
+                    <div class="auto-btn4"></div>
+                </div>
 
-<!-- Manual nav start -->
-<div class="navgate-manual">
-<label for="radio1" class="manual-btn"></label>
-<label for="radio2" class="manual-btn"></label>
-<label for="radio3" class="manual-btn"></label>
-<label for="radio4" class="manual-btn"></label>
-</div>
+                <!-- Manual nav start -->
+                <div class="navgate-manual">
+                    <label for="radio1" class="manual-btn"></label>
+                    <label for="radio2" class="manual-btn"></label>
+                    <label for="radio3" class="manual-btn"></label>
+                    <label for="radio4" class="manual-btn"></label>
+                </div>
 
-</div>
-</div>
-</main>
-
+            </div>
+        </div>
+    </main>
 
     <main class="page landing-page">
-        <section class="clean-block clean-hero" style="color: rgba(9, 162, 255, 0.7)">
-            <div class="text">
-                <h2>Online Birth Certificate System</h2>
-                <p>
-                    This is an online portal for generating Birth Certificate.Avoid those long queues and get certificate online through mail.
-                </p>
-                <button class="btn btn-outline-light btn-lg" type="button" onclick="window.location.href = '#learn-more'">
-                    Learn More
-                </button>
-            </div>
-        </section>
         <section class="clean-block clean-info dark" id="learn-more">
             <div class="container">
                 <div class="block-heading">
@@ -107,7 +94,7 @@
                                 If you wish to generate a birth - certicate, or regenerate a lost certificate or do necessary changes /modifications in the certificate, Login through the User Panel.
                             </p>
                         </div>
-                        <button class="btn btn-outline-primary btn-lg" type="button" onclick="window.location.href = 'login.html';">
+                        <button class="btn btn-outline-primary btn-lg" type="button" onclick="window.location.href = 'login_form.php';">
                             Login Now
                         </button>
                     </div>
@@ -118,7 +105,7 @@
                                 If you are a government administrator, the you can view all the applications, the verified applications and the rejected applications.You must Login through the Admin Panel to manage the system.
                             </p>
                         </div>
-                        <button class="btn btn-outline-primary btn-lg" type="button ">Login Now</button>
+                        <button class="btn btn-outline-primary btn-lg" type="button" onclick="window.location.href = 'login_form.php';">Login Now</button>
                     </div>
                     <div class="col-md-6">
                         <img class="img-thumbnail" src="assets/img/scenery/admin.png " />
