8ff0020f1789762a5b3a13e9a229d894f9afac0a
Soniya <soniya.n@somaiya.edu>
2022-01-04 17:18:57 +0530

    :memo: List of States where Online birth certificate generation service is available

---
+++ states.php
@@ -0,0 +1,260 @@
+<DOCTYPE html>
+    <html>
+
+    <head>
+        <title>States</title>
+        <style>
+            body {
+                
+                    font: 16px/1.45 'Montserrat', sans-serif;
+                
+            }
+
+            .box {
+                width: 1065px;
+                height: 2150px;
+                margin: 40px auto;
+                background: #fff;
+                border-radius: 7px;
+                box-shadow: 1px 2px 4px rgba(0, 0, 0, .3);
+            }
+
+            .box .heading {
+                background: black;
+                border-radius: 7px 7px 0px 0px;
+                padding: 10px;
+                color:white;
+                text-align: center;
+                font-family: "Rubik";
+                font-size: 20px;
+            }
+            .one-column {
+                            width: auto !important;
+                            float: none !important;
+                        }
+
+                        
+            .states {
+                padding: 0px 20px 20px;
+                
+            }
+            
+            .cols.col {
+                            width: 100%;
+                            
+                        }
+
+            
+                        
+
+            .aligncenter {
+                          text-align: center;
+                          
+                        }
+
+            .state-list {
+                        display: inline-block;
+                        color: #068cfe;
+                        background: 000;
+                        text-decoration: none;
+                        text-align: centre;
+                        font-size: 16px;
+                        padding: 12px;
+                        width: 40%;
+                        position: static;
+                        border: 1px solid #068cfe;
+                        margin-bottom: 20px;
+                        font-weight: 600;
+                        }
+                        hover{
+                                        background-color:black;
+                                         }
+                        
+
+        </style>
+        <div class="box">
+            <p class="heading">States</p>
+            <div class="states">
+            <div class="cols cols2">
+        <div id="primary" class="content-area one-column">
+            <div id="content" class="site-content"> 
+                <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Andhra Pradesh</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Arunachal Pradesh</a>
+        </div>
+        </div>
+        
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Assam</a>
+        </div>
+        </div>
+        
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Bihar</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Chhattisgarh</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Delhi</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Goa</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Gujarat</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Haryana</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Himachal Pradesh </a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Jammu and Kashmir</a>
+        </div>
+        </div>
+
+<div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Jharkhand </a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Karnataka </a>
+        </div>
+        </div>
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Kerala</a>
+        </div>
+        </div>   
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Madhya Pradesh</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Maharashtra</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Manipur</a>
+        </div>
+        </div>  
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Meghalaya</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Mizoram</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Nagaland</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Odisha</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Punjab</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Rajasthan </a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Sikkim</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Tamil Nadu</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Telangana</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Tripura</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Uttar Pradesh</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">Uttarakhand</a>
+        </div>
+        </div>
+
+        <div class="cols cols2">
+        <div class="col aligncenter">
+            <a class="state-list" href=" ">West Bengal</a>
+        </div>
+        </div>
+        
+       </html> 
