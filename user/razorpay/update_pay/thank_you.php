71395140a7dc49f44dd33b7dd119f8bde74b157a
Sanika <sanika.dk@somaiya.edu>
2022-01-16 23:05:17 +0530

    Corrected redirect link

--- user/razorpay/update_pay/thank_you.php
+++ user/razorpay/update_pay/thank_you.php
@@ -1,2 +1,2 @@
 <h1 style="text-align:center;color:dodgerblue">Payment Successfull. Thank You!!</h1><br><br>
-<center><a href = "../../../applid.php">Click Here to Update Application Form</a></center>
+<center><a href="../../applid.php">Click Here to Update Application Form</a></center>
