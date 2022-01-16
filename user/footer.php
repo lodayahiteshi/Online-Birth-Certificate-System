7c30d48da653d5364abd8234f7cbe5ad8d63e920
Sanika <sanika.dk@somaiya.edu>
2022-01-16 23:13:05 +0530

    Updated links

---
+++ user/footer.php
@@ -0,0 +1,42 @@
+<footer class="page-footer dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <h5>Get started</h5>
+                <ul>
+                    <li><a href="./index.php"> Home</a></li>
+                    <li><a href="./new-application.php">Generate Certificate</a></li>
+                    <li><a href="./certificate/view-applid.php">View Certificate/</a><a href="./certificate/applid.php">Download Certificate</a></li>
+                </ul>
+            </div>
+
+            <div class="col-sm-3">
+                <h5>Contact us</h5>
+                <ul>
+                    <li><span><i class="fa fa-envelope" style="color:grey;"></i><a href="mailto:info.birthcertificate.services@gmail.com"> info.birthcertificate.services@gmail.com</a></span></li>
+                    <li><span><i class="fa fa-phone" style="color:grey;"></i><a href="tel:+919987709909"> +91 99877 09909</a></span></li>
+                </ul>
+            </div>
+            <div class="col-sm-3"></div>
+            <div class="col-sm-3">
+                <h5>Legal</h5>
+                <ul>
+                    <li><a href="./terms-and-conditions.php">Terms and Conditions </a></li>
+                    <li><a href="./privacy-policy.php">Privacy Policy</a></li>
+                    <li><a href="./faq.php">FAQ</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <p>© <script>
+                document.write(new Date().getFullYear())
+            </script> Online Birth Certificate Generator</p>
+    </div>
+</footer>
+
+<!-- End: Footer Dark -->
+<script src="assets/bootstrap/js/bootstrap.min.js "></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js "></script>
+<script src="assets/js/vanilla-zoom.js "></script>
+<script src="assets/js/theme.js "></script>
