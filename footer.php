b33e47bb643a7e34162ffa73bb0dabda2ce830a4
Sanika <sanika.dk@somaiya.edu>
2022-01-16 23:07:04 +0530

    Updated links

--- footer.php
+++ footer.php
@@ -1,42 +1,37 @@
 <footer class="page-footer dark">
     <div class="container">
         <div class="row">
-            <div class="col-sm-3">
+            <div class="col-sm-4">
                 <h5>Get started</h5>
                 <ul>
                     <li><a href="./index.php"> Home</a></li>
-                    <li><a href="user/certificate/applid.php">Generate/Download Certificate</a></li>
-                    <li><a href="user/certificate/applid.php">View Certificate</a></li>
+                    <li><a href="user/new-application.php">Generate Certificate</a></li>
+                    <li><a href="user/certificate/view-applid.php">View Certificate/</a><a href="user/certificate/applid.php">Download Certificate</a></li>
                 </ul>
             </div>
 
-            <div class="col-sm-3">
+            <div class="col-sm-4">
                 <h5>Contact us</h5>
                 <ul>
-                    <li><span><i class="fa fa-envelope"></i><a href="mailto:06sanika03@gmail.com"> info@birthcertificate.in</a></span></li>
-                    <li><span><i class="fa fa-phone"></i><a href="tel:+919987709909"> +91 99877 09909</a></span></li>
+                    <li><span><i class="fa fa-envelope" style="color:grey;"></i><a href="mailto:info.birthcertificate.services@gmail.com"> info.birthcertificate.services@gmail.com</a></span></li>
+                    <li><span><i class="fa fa-phone" style="color:grey;"></i><a href="tel:+919987709909"> +91 99877 09909</a></span></li>
                 </ul>
             </div>
-            <div class="col-sm-3">
-                <h5>Support</h5>
-                <ul>
-                    <li><a href="./faq.php">FAQ</a></li>
-                    <li><a href="#">Help desk </a></li>
-                    <li><a href="#">Forums </a></li>
-                </ul>
-            </div>
-            <div class="col-sm-3">
+            <div class="col-sm-4">
                 <h5>Legal</h5>
                 <ul>
                     <li><a href="./terms-and-conditions.php">Terms and Conditions </a></li>
                     <li><a href="./privacy-policy.php">Privacy Policy</a></li>
-                    <li><a href="#">Refund Policy</a></li>
+                    <li><a href="./user/faq.php">FAQ</a></li>
                 </ul>
             </div>
         </div>
     </div>
     <div class="footer-copyright">
-        <p>©2021 Online Birth Certificate Generator</p>
+        <p>© <script>
+                document.write(new Date().getFullYear())
+            </script> Online Birth Certificate Generator</p>
+
     </div>
 </footer>
 
